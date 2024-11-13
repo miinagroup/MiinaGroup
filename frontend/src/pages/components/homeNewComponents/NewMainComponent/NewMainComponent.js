5777174757d2731425931cfd8925ad341a6284f0
Maria <131111011+TechteamCTL@users.noreply.github.com>
2024-11-13 14:38:40 +0800

    updated video links

--- frontend/src/pages/components/homeNewComponents/NewMainComponent/NewMainComponent.js
+++ frontend/src/pages/components/homeNewComponents/NewMainComponent/NewMainComponent.js
@@ -3,8 +3,8 @@ import styles from "./NewMainComponent.module.css";
 const NewMainComponent = () => {
     return <div>
     <div className={styles.homeNew}>
-      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} desktop`}><source src={"https://ctladmin.b-cdn.net/video/Sequence%2001.mp4"} type="video/mp4" /></video>
-      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} mobile`}><source src={"https://ctladmin.b-cdn.net/video/Phone.mp4"} type="video/mp4" /></video>
+      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} desktop`}><source src={"https://ctladmin.b-cdn.net/video/Horizontal.CTL.main.mp4"} type="video/mp4" /></video>
+      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} mobile`}><source src={"https://ctladmin.b-cdn.net/video/Vertical.CTL.main.mp4"} type="video/mp4" /></video>
     </div> 
   </div>
 }
