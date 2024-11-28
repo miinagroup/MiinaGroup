29829a354d789217de29344ad2abdbe06a248b4c
Maria <131111011+TechteamCTL@users.noreply.github.com>
2024-11-28 16:25:04 +0800

    changed video visibility

--- frontend/src/pages/components/homeNewComponents/NewMainComponent/NewMainComponent.js
+++ frontend/src/pages/components/homeNewComponents/NewMainComponent/NewMainComponent.js
@@ -4,7 +4,7 @@ const NewMainComponent = () => {
     return <div>
     <div className={styles.homeNew}>
       <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} desktop`}><source src={"https://ctladmin.b-cdn.net/video/Horizontal.CTL.main%20(1).mp4"} type="video/mp4" /></video>
-      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} mobile`}><source src={"https://ctladmin.b-cdn.net/video/Vertical.CTL.main%20(1).mp4"} type="video/mp4" /></video>
+      <video autoPlay loop muted playsInline preload="auto" className={`${styles.bgVid} ${styles.mobile}`}><source src={"https://ctladmin.b-cdn.net/video/Vertical.CTL.main%20(1).mp4"} type="video/mp4" /></video>
     </div> 
   </div>
 }
