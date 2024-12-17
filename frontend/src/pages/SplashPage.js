3f1d814130c7386afed6d6fa8397d802f2319d23
Maria <131111011+TechteamCTL@users.noreply.github.com>
2024-12-17 11:25:50 +0800

    changed layout for splashpage

--- frontend/src/pages/SplashPage.js
+++ frontend/src/pages/SplashPage.js
@@ -1,23 +1,14 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
 import LoginRegisterPage from "./LoginRegisterPage";
 import "./SplashPage.css";
 
 function SplashPage() {
   return (
     <>
-      <div className="splashpage">
-        <img src="./images/grayAUS-CTL.png" alt="" className="redAUS"></img>
-
-        <Container className="center_container">
-          {/*                 <div>
-                <img src="./images/CTL-hex.png" alt="" className="rotate logo"></img>
-                </div> */}
-
-          <div className="loginReg">
+      <div className="splashpagewrapper">
+          <div className="loginRegWrapper">
             <LoginRegisterPage />
           </div>
-        </Container>
       </div>
     </>
   );
