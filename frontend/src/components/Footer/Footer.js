47ef61e39ebbf39bcaeea6d57080fea5ee108678
PRAVEEN <techteam@ctlservices.com.au>
2025-02-06 14:36:18 +0800

    Update Footer.js

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,22 +9,22 @@ const Footer = () => {
                     <div>
                         <h6>POLICIES</h6>
                         <Nav.Link
-                                id="terms_conditions"
-                                className="ft_c"
-                                href="/TermsConditions"
-                                target="_blank"
-                                >
-                                Terms and Conditions
-                                </Nav.Link>
-                                <Nav.Link
-                                id="privacy_policy"
-                                className="ft_c"
-                                href="/privacypolicy"
-                                target="_blank"
-                                >
-                                Privacy Policy
-                                </Nav.Link>
-                                <Nav.Link
+                            id="terms_conditions"
+                            className="ft_c"
+                            href="/TermsConditions"
+                            target="_blank"
+                        >
+                            Terms and Conditions
+                        </Nav.Link>
+                        <Nav.Link
+                            id="privacy_policy"
+                            className="ft_c"
+                            href="/privacypolicy"
+                            target="_blank"
+                        >
+                            Privacy Policy
+                        </Nav.Link>
+                        {/* <Nav.Link
                                 id="indigenous-procurement"
                                 href="/pdfpreview?/Indigenous%20Procurement%20Policy.pdf"
                                 target="_blank"
@@ -32,69 +32,69 @@ const Footer = () => {
                                 >
                                 Indigenous Procurement Policy
                                 <i className="bi bi-file-earmark-pdf-fill"></i>
-                                </Nav.Link>
+                                </Nav.Link> */}
                     </div>
 
                     <div>
                         <h6>SERVICES</h6>
                         <Nav.Link
-                                id="goods_return"
-                                className="ft_c"
-                                // href="/goodsreturnform"
-                                target="_blank"
-                                >
-                                Warranty
-                                </Nav.Link>
-                                <Nav.Link
-                                id="goods_return"
-                                className="ft_c"
-                                href="/pdfpreview?/Miina_Credit-Application-Form.pdf"
-                                target="_blank"
-                                >
-                                Credit Application form
-                                <i className="bi bi-file-earmark-pdf-fill"></i>
-                                </Nav.Link>
-                                <Nav.Link
-                                id="goods_return"
-                                className="ft_c"
-                                // href="/pdfpreview?/CTL_Australia_Supplier_Code_of_Conduct.pdf"
-                                target="_blank"
-                                >
-                                Supplier Code of Conduct
-                                <i className="bi bi-file-earmark-pdf-fill"></i>
-                                </Nav.Link>
-                    </div>  
+                            id="goods_return"
+                            className="ft_c"
+                            // href="/goodsreturnform"
+                            target="_blank"
+                        >
+                            Warranty
+                        </Nav.Link>
+                        <Nav.Link
+                            id="goods_return"
+                            className="ft_c"
+                            href="/pdfpreview?/Miina_Credit-Application-Form.pdf"
+                            target="_blank"
+                        >
+                            Credit Application form
+                            <i className="bi bi-file-earmark-pdf-fill"></i>
+                        </Nav.Link>
+                        <Nav.Link
+                            id="goods_return"
+                            className="ft_c"
+                            // href="/pdfpreview?/CTL_Australia_Supplier_Code_of_Conduct.pdf"
+                            target="_blank"
+                        >
+                            Supplier Code of Conduct
+                            <i className="bi bi-file-earmark-pdf-fill"></i>
+                        </Nav.Link>
+                    </div>
 
                     <div>
                         <h6>STATEMENTS</h6>
                         <Nav.Link
-                                id="goods_return"
-                                className="ft_c"
-                                href="/supplierstatement"
-                                target="_blank"
-                                >
-                                Supplier Commitment
-                                </Nav.Link>
-                    </div> 
-                        <div>
+                            id="goods_return"
+                            className="ft_c"
+                            href="/supplierstatement"
+                            target="_blank"
+                        >
+                            Supplier Commitment
+                        </Nav.Link>
+                    </div>
+                    <div>
                         <h6>CONTACTS</h6>
                         <Nav.Link
                             id="contact_sales"
                             className="ft_c"
                             href="mailto:howard@miinagroup.com.au"
-                            >
+                        >
                             howard@miinagroup.com.au
-                            </Nav.Link>
-                            <Nav.Link
+                        </Nav.Link>
+                        <Nav.Link
                             id="contact_accounts"
                             className="ft_c"
                             href="mailto:admin@miinagroup.com.au"
-                            >
+                        >
                             admin@miinagroup.com.au
-                            </Nav.Link>
-                    </div>   
-        </div>
-        </div>
+                        </Nav.Link>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className={styles.copyright}>Copyright © Miina Group 2025</div>
     </div>
